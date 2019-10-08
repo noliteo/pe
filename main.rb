029f01f506e9a8507f92c3aa7812df1e1b5b72df
Taehun Kim <tkim@assurance.com>
2019-10-07 19:59:13 -0700

    pe20

---
+++ main.rb
@@ -0,0 +1,4 @@
+require './solution'
+include Solution
+
+Solution.problem20()
---
+++ solution.rb
@@ -0,0 +1,27 @@
+module Solution
+
+# https://projecteuler.net/problem=20
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+  def problem20(input=100)
+    p "hi"
+    res, ans = 1, 0
+
+    (1..input).each do |n|
+      res=res*n
+    end
+
+    p "#{input}! is #{res}"
+
+    res.to_s.split("").map{ |i| i.to_i }.each do |n|
+      ans+=n
+    end
+
+    p "RESULT: #{ans}"
+
+  end
+
+end
